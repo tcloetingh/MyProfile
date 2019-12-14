0a360db4090370699acecf7e63c5fd3a4531488e
Tyler Cloetingh <tcloetingh@gmail.com>
2019-12-14 16:56:19 -0500

    edit and update build out

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,36 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end 
 
+  def new
+    @port_item = Portfolio.new 
+  end
+
+  def create
+    @port_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @port_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @port_edit_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @port_edit_update = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @port_edit_update.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
 end
