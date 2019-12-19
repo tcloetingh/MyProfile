2b320c2670e46991c2644779c1ccb3430a9f27c2
Tyler Cloetingh <tcloetingh@gmail.com>
2019-12-19 08:56:17 -0500

    refactor of sessions feature into controller concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
-
-  before_action :set_source
-
-  def set_source
-    session[:setsource] = params[:q] if params[:q] 
-  end 
+  include SetSource
 end
