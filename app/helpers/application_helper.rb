c0e7acb8d964e5805b7343cf3e0e7a075844161e
Tyler Cloetingh <tcloetingh@gmail.com>
2019-12-27 11:28:21 -0500

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,44 @@ module ApplicationHelper
   def set_copyright
     CloetinghViewTool::Renderer.copyright('Tyler Cloetingh', 'All rights reserved')
   end 
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_path,
+        title: "About"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      }
+
+    ]
+
+  end
+
+  def nav_helper(css_style, tag_type)
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{css_style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>" 
+    end 
+    nav_links.html_safe  
+  end
+
+  def active?(path)
+    "active" if current_page? path
+  end
 end
