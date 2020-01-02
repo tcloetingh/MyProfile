df10dea7506bc05d76cd1a132a49d0e403fd11c8
Tyler Cloetingh <tcloetingh@gmail.com>
2020-01-02 08:56:50 -0500

    built initial form and partial call for comments along with controller actions

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,7 +6,7 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body 
   
   belongs_to :topic, optional: true
-  has_many :comments, :dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def self.special_blogs 
     all
