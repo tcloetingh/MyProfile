99123b1cafd75d628d988bf2e12b6331e3451748
Tyler Cloetingh <tcloetingh@gmail.com>
2020-01-06 16:07:26 -0500

    configured action cable and redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://tyler-c-portfolio.herokuapp.com/', 'https://tyler-c-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://tyler-c-profile.herokuapp.com/cable"
 end
